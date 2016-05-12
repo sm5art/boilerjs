718e1543b44bb860a32761e3be0a8f4fd0cb336a
Artur Kashperskiy <art@network.ru>
2016-05-12 00:10:37 -0700

    fixed gulp file to properly minify production code

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,14 @@
-var gulp = require('gulp');
-var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var browserify = require('gulp-browserify')
+var uglify = require('gulp-uglify');
+var gulp = require('gulp');
 
-gulp.task('scripts', function () {
-
-    gulp.src(['client/*.js'])
-        .pipe(browserify({
-            debug: true,
-            transform: [ 'reactify' ]
-        }))
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('./public/js/'));
-
+gulp.task('default', function () {
+  gulp.src('client/*.js')
+  .pipe(browserify({transform:['reactify']}))
+  .pipe(babel({presets:['es2015'],compact:false}))
+  .pipe(concat('all.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/js'))
 });
-
-gulp.task('default', ['scripts']);
